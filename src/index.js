6f640c38127fb6937e426f305a0e8b6c4bea66f2
unknown <aditisuman5480@gmail.com>
2023-01-31 22:48:57 +0530

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ----------------- Second HOmework -----------------
+const cTime = new Date().toLocaleTimeString();
+const cDate = new Date().toLocaleDateString();
+ReactDOM.render(
+    <div>
+        <h1>Hii,I am Aditi</h1>
+        <p>Here is the current time {cTime}</p>
+        <p>Here is the current date {cDate}</p>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </div>,
+    document.getElementById('root')
+)
+
+// ----------------- Function -----------------
+// const name = 'Aditi'
+// ReactDOM.render(
+//     <div>
+//         <p>Hii i am {name}</p>
+//         <p>add 2 + 3  is {2 + 3}</p>
+//     </div>, document.getElementById('root'
+//     ))
+
+// ----------------- First Homework -----------------
+// ReactDOM.render(
+//     <div>
+//         <h1>Netflix series</h1>
+//         <p>List of 5 best series</p>
+//         <ol>
+//             <li>Wednesday</li>
+//             <li>Stranger things</li>
+//             <li>Mr Robort</li>
+//             <li>Dark</li>
+//             <li>Squid games</li>
+//         </ol>
+//     </div>,
+//     document.getElementById('root'))
+
+// ----------------- In form of array -----------------
+// ReactDOM.render(
+//     [
+//         <h1>Hello</h1>,
+//         <h2>i am Adiit</h2>
+//     ],
+//     document.getElementById('root'))
+
+// ----------------- With the help of div, we can write multiple jsx -----------------
+    // ReactDOM.render(
+    //     <div>
+    //         <h1>Hello</h1>
+    //         <h2>i am Adiit</h2>
+    //     </div>,
+    //     document.getElementById('root'))
+
+//----------------- JSX -----------------
+// ReactDOM.render(<h1>Hello world</h1>,
+//     document.getElementById('root'))
