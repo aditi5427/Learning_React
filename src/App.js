46cea5c09bf86fa9d1e4c0bf545bb840993ca802
unknown <aditisuman5480@gmail.com>
2023-03-14 22:55:28 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,27 +2,34 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
-    const [name, setName] = useState()
-    const [fullName, setfullname] = useState();
+    const [inputList, setInputList] = useState(" ");
+    const [item, setItem] = useState([]);
 
-    const innerEvent = (event) => {
-        console.log(event.target.value);
-        setName(event.target.value);
-    }
+    const itemEvent = (event) => {
+        setInputList(event.target.value);
+    };
 
-    const onSubmit = () => {
-        setfullname(name);
-    }
-
+    const listItem = () => {
+        setItem((oldItem) => {
+            return [...oldItem, inputList];
+        });
+        setInputList("  ");
+    };
 
     return (
         <>
-            <h1>hello {fullName}</h1>
-            <input type='text' placeholder='Enter youe name' onChange={innerEvent} />
-            <button onClick={onSubmit} >Click</button>
+            <div className='toDo'>
+                <input className='addList' type='text' placeholder='Add to-do list' value={inputList} onChange={itemEvent} />
+                <button className='btn' onClick={listItem}> + </button>
+                <ol>
+                    {item.map((itemVal) => {
+                        return <li>{itemVal}</li>
+                    })
+                    }
+                </ol>
+            </div>
         </>
     )
-};
+}
 
 export default App;
-
--- src/index.js
+++ src/index.js
@@ -16,6 +16,30 @@ root.render(
   );
 
 
+//   const App = () => {
+//     const [name, setName] = useState()
+//     const [fullName, setfullname] = useState();
+
+//     const innerEvent = (event) => {
+//         console.log(event.target.value);
+//         setName(event.target.value);
+//     }
+
+//     const onSubmit = () => {
+//         setfullname(name);
+//     }
+
+
+//     return (
+//         <>
+//             <h1>hello {fullName}</h1>
+//             <input type='text' placeholder='Enter youe name' onChange={innerEvent} />
+//             <button onClick={onSubmit} >Click</button>
+//         </>
+//     )
+// };
+
+
 //-------Hooks and Events------------
 //   import React, { useState } from 'react';
 // import './App.css';
